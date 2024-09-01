8fa93c5d56322464e15fc57c653df2285f153111
June Rhomel <junemandigma@gmail.com>
2024-09-01 23:27:56 +0800

    test

--- src/mongodb/database_connection.monggodb.ts
+++ src/mongodb/database_connection.monggodb.ts
@@ -5,6 +5,7 @@ import { GetSecret } from "../application/config/secret";
 const MongoDbClient = async () => {
     const secret = await GetSecret();
     const DB = secret.connectionString;
+    console.log(DB)
     const client = new MongoClient(DB, {
         serverApi: {
             version: ServerApiVersion.v1,
